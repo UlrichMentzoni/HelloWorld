a452f6de993e36b05d2d794d69ac0562bb72ba2f
UlrichMentzoni <ulrich.mentzoni@gmail.com>
2014-08-07 21:57:13 +0200

    Initial commit

---
+++ src/no/um/main/Program.java
@@ -0,0 +1,12 @@
+package no.um.main;
+
+//Importerer alle nødvendige biblioteker
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!!");
+		
+	}
+}
